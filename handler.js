75c52839da7cc3d7df513f5ea50b1ce9a2188df3
Alexandra <alexandrajn17@gmail.com>
2020-02-24 17:57:42 +0000

    External config for DB connection

--- handler.js
+++ handler.js
@@ -7,10 +7,10 @@ const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'bbc-rds-attempt2.c0fymvtwgwia.eu-west-2.rds.amazonaws.com',
-  user: '------------',
-  password: '--------------',
-  database: '-------------------------'
+  host: 'process.env.DB_HOST',
+  user: 'process.env.DB_USER',
+  password: 'process.env.DB_PASSWORD',
+  database: 'process.env.DB_SCHEMA'
 });
 
 app.get('/tasks', function (req, res) {
