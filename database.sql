8a7f5c45a9d4abcef7901d9677c28f87f5b8c35d
Alexandra <alexandrajn17@gmail.com>
2020-02-24 15:52:48 +0000

    NEW DATABASE DUMP - on correct server

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.19, for osx10.15 (x86_64)
 --
--- Host: bbc-course-rds.cgeafo66y31v.us-east-2.rds.amazonaws.com    Database: tasks_todo
+-- Host: bbc-rds-attempt2.c0fymvtwgwia.eu-west-2.rds.amazonaws.com    Database: lotus_flow
 -- ------------------------------------------------------
 -- Server version	5.7.22
 
@@ -16,31 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `tasks`
+-- Table structure for table `task`
 --
 
-DROP TABLE IF EXISTS `tasks`;
+DROP TABLE IF EXISTS `task`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tasks` (
+CREATE TABLE `task` (
   `taskID` varchar(50) NOT NULL,
   `description` varchar(200) NOT NULL,
   `completed` tinyint(1) NOT NULL,
   `user_id` varchar(50) NOT NULL,
   PRIMARY KEY (`taskID`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userID`)
+  CONSTRAINT `task_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tasks`
+-- Dumping data for table `task`
 --
 
-LOCK TABLES `tasks` WRITE;
-/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES ('1b671a64-40d5-491e-99b0-da01ff1f3341','book a yoga class',0,'2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d');
-/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES ('1dc69d24-b414-4469-8745-e8f9af74637a','Mow the lawn',0,'caba5eba-5d18-4986-8be2-6efcec65a061'),('2a051aed-45dc-482d-a092-f5611ff5c53c','Take out the trash',0,'9fbe5ebe-5489-4ee9-a04e-d2ee1ec1197f'),('a8cc7ebc-311b-4604-8e37-5db4f67dbe36','Get the groceries',0,'7096d2b7-e612-4b44-a9e2-8e29fc9bed69');
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -63,7 +63,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d','alex@bbc.co.uk');
+INSERT INTO `user` VALUES ('7096d2b7-e612-4b44-a9e2-8e29fc9bed69','steph@bbc.co.uk'),('9fbe5ebe-5489-4ee9-a04e-d2ee1ec1197f','jamie@bbc.co.uk'),('caba5eba-5d18-4986-8be2-6efcec65a061','alex@bbc.co.uk');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -76,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-24 12:50:25
+-- Dump completed on 2020-02-24 15:52:19
