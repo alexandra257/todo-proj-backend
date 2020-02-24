28e16ec1cb028c4eb96019a53a07301b3776b081
Alexandra <alexandrajn17@gmail.com>
2020-02-24 11:23:46 +0000

    JSON response for tasks

--- handler.js
+++ handler.js
@@ -2,14 +2,16 @@
 
 const serverless = require('serverless-http');
 const express = require('express');
-const app = express()
+const app = express();
 
 
 app.get('/tasks', function (req, res) {
-  res.send('Hello World!');
-})
+  res.json({
+    message: 'Your API works',
+  });
+});
 
-// module.exports.handler = serverless(app);
+module.exports.tasks = serverless(app);
 
 
 
