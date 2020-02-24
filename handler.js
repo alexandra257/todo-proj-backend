caab522da04711dc661e50322fab8d4be8b18143
Alexandra <alexandrajn17@gmail.com>
2020-02-24 17:06:43 +0000

    complete backend API ready for GET, POST, PUT and DELETE

--- handler.js
+++ handler.js
@@ -4,37 +4,78 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const uuidv4 = require('uuid/v4');
-var mysql = require('mysql');
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: 'bbc-rds-attempt2.c0fymvtwgwia.eu-west-2.rds.amazonaws.com',
+  user: '------------',
+  password: '--------------',
+  database: '-------------------------'
+});
 
 app.get('/tasks', function (req, res) {
-  res.json({
-    tasks: {
-      taskList: [
-        {
-          id: uuidv4(),
-          description: 'Learn serverless!',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          description: 'Understand APIs',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          description: 'Meditate',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          description: 'Order a pizza',
-          completed: false,
-        },
-      ]
+
+  connection.query('SELECT * FROM `task` WHERE `user_id` = "7096d2b7-e612-4b44-a9e2-8e29fc9bed69"', function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    if (error) {
+      console.log("Your query had a problem with fetching the tasks", error);
+      res.status(500).json({ errorMessage: error });
     }
+    else {
+      //Query worked!
+      res.json({ tasks: results })
+    }
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
   });
 });
+
+
+// const taskList = [
+//   {
+//     taskID: uuidv4(),
+//     description: 'Learn serverless!',
+//     completed: false,
+//   },
+//   {
+//     taskID: uuidv4(),
+//     description: 'Understand APIs',
+//     completed: false,
+//   },
+//   {
+//     taskID: uuidv4(),
+//     description: 'Meditate',
+//     completed: false,
+//   },
+//   {
+//     taskID: uuidv4(),
+//     description: 'Order a pizza',
+//     completed: false,
+//   }
+// ]
+
+
+//creating tasks
+app.post('/tasks', function (req, res) {
+  res.json({
+    message: 'Your POST works',
+  });
+});
+//Updating tasks
+app.put('/tasks/:taskID', function (req, res) {
+  res.json({
+    message: 'Your PUT works',
+  });
+});
+
+//Deleting tasks
+app.delete('/tasks/:taskID', function (req, res) {
+  res.json({
+    message: 'Your DELETE works',
+  });
+});
+
+
 
 module.exports.tasks = serverless(app);
 
