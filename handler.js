50340da3e6f6c52f234ecb01e4a324724615a2a9
Alexandra <alexandrajn17@gmail.com>
2020-05-19 13:07:44 +0100

    value of completed in PUT request changed to 1

--- handler.js
+++ handler.js
@@ -1,42 +1,41 @@
-'use strict';
+"use strict";
+const serverless = require("serverless-http");
+const express = require("express");
+const uuidv4 = require("uuid/v4");
+const mysql = require("mysql");
+const cors = require("cors");
 
-const serverless = require('serverless-http');
-const express = require('express');
 const app = express();
-app.use(express.json()); //takes JSON from the client & converts it to a JS object
-const uuidv4 = require('uuid/v4');
-const mysql = require('mysql');
-const cors = require('cors');
+app.use(express.json());
 app.use(cors());
-
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_SCHEMA
+  database: process.env.DB_SCHEMA,
 });
 
-app.get('/tasks', function (req, res) {
-
-  connection.query('SELECT * FROM `task` WHERE `user_id` = "7096d2b7-e612-4b44-a9e2-8e29fc9bed69"', function (error, results, fields) {
-    // error will be an Error if one occurred during the query
-    if (error) {
-      console.log("Your query had a problem with fetching the tasks", error);
-      res.status(500).json({ errorMessage: error });
+app.get("/tasks", function (req, res) {
+  connection.query(
+    'SELECT * FROM `task` WHERE `user_id` = "7096d2b7-e612-4b44-a9e2-8e29fc9bed69"',
+    function (error, results, fields) {
+      // error will be an Error if one occurred during the query
+      if (error) {
+        console.log("Your query had a problem with fetching the tasks", error);
+        res.status(500).json({ errorMessage: error });
+      } else {
+        //Query worked!
+        res.json({ tasks: results });
+      }
+      // results will contain the results of the query
+      // fields will contain information about the returned results fields (if any)
     }
-    else {
-      //Query worked!
-      res.json({ tasks: results })
-    }
-    // results will contain the results of the query
-    // fields will contain information about the returned results fields (if any)
-  });
+  );
 });
 
 //creating tasks
-app.post('/tasks', function (req, res) {
-
+app.post("/tasks", function (req, res) {
   //Accept information from the client
   //about what task if being created
 
@@ -48,15 +47,18 @@ app.post('/tasks', function (req, res) {
 
   //Take that information and pre-poulate a SQL INSERT statement
   ///Execute the statement
-  connection.query('INSERT INTO `task` SET ?', taskToInsert, function (error, results, fields) {
+  connection.query("INSERT INTO `task` SET ?", taskToInsert, function (
+    error,
+    results,
+    fields
+  ) {
     if (error) {
       console.log("Your query had a problem with inserting a new task", error);
       res.status(500).json({ errorMessage: error });
-    }
-    else {
+    } else {
       //Return to the client information about the task that has been created
       res.json({
-        task: taskToInsert
+        task: taskToInsert,
       });
     }
     // results will contain the results of the query
@@ -64,16 +66,16 @@ app.post('/tasks', function (req, res) {
   });
 });
 
-
-
-app.put('/tasks/:taskID', function (req, res) {
+app.put("/tasks/:taskID", function (req, res) {
   const taskUpdated = req.params.taskID;
 
-  // "This PUT edits / creates a new task if the ID doesn't exist"	  // set completed to 1 where id = id
-  connection.query('UPDATE `task` SET `completed` = ? WHERE `taskID` = ?', [true, taskUpdated],
+  // this PUT edits an existing task	  // set completed to 1 where id = id
+  connection.query(
+    "UPDATE `task` SET `completed` = ? WHERE `taskID` = ?",
+    [1, taskUpdated],
     function (error, results, fields) {
       if (error) {
-        console.error('Could not mark tasks as updated', error);
+        console.error("Could not mark tasks as updated", error);
         res.status(500).json({ errorMessage: error });
       } else {
         res.json({ task: taskUpdated });
@@ -83,29 +85,23 @@ app.put('/tasks/:taskID', function (req, res) {
   );
 });
 
-
-// Updating tasks
-// app.put('/tasks/:taskID', function (req, res) {
-//   res.json({
-//     message: 'Your PUT works',
-//   });
-// });
-
-
 //Deleting tasks
-app.delete('/tasks/:taskID', function (req, res) {
+app.delete("/tasks/:taskID", function (req, res) {
   const taskId = req.params.taskID;
 
-  connection.query('DELETE FROM `task` WHERE `taskID` = ?', taskId, function (error, results, fields) {
+  connection.query("DELETE FROM `task` WHERE `taskID` = ?", taskId, function (
+    error,
+    results,
+    fields
+  ) {
     // error will be an Error if one occurred during the query
     if (error) {
       console.log("Your query had a problem with DELETING the task", error);
       res.status(500).json({ errorMessage: error });
-    }
-    else {
+    } else {
       //Query worked!
       res.json({
-        tasks: results
+        tasks: results,
       });
     }
     // results will contain the results of the query
@@ -113,29 +109,4 @@ app.delete('/tasks/:taskID', function (req, res) {
   });
 });
 
-
-
 module.exports.tasks = serverless(app);
-
-
-
-
-
-
-
-// module.exports.tasks = async event => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(
-//       {
-//         message: 'Your API works',
-//       },
-//       null,
-//       2
-//     ),
-//   };
-
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
